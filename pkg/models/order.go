77e9bcb37855da5949533ca55dff7886311dacbf
brain <vaibhav.satam@accionlabs.com>
2022-12-16 13:39:18 +0530

    added order create example

--- pkg/models/order.go
+++ pkg/models/order.go
@@ -2,27 +2,25 @@ package models
 
 import (
 	"context"
-	"fmt"
-	"time"
+	"errors"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/google/uuid"
 	"github.com/satioO/order-mgmt/pkg/pb"
 	"github.com/spf13/viper"
 )
 
 type Product struct {
-	ProductID   int32  `dynamodbav:"productId"`
+	ProductID   uint32 `dynamodbav:"productId"`
 	ProductName string `dynamodbav:"productName"`
 }
 
 type Order struct {
 	PK               string  `dynamodbav:"PK"`
 	SK               string  `dynamodbav:"SK"`
-	CustomerID       int32   `dynamodbav:"customerId"`
-	SellerID         int32   `dynamodbav:"sellerId"`
+	CustomerID       uint32  `dynamodbav:"customerId"`
+	SellerID         uint32  `dynamodbav:"sellerId"`
 	Product          Product `dynamodbav:"product"`
 	PaymentMethod    string  `dynamodbav:"paymentMethod"`
 	DeliveryLocation string  `dynamodbav:"deliveryLocation"`
@@ -39,29 +37,11 @@ func NewOrderRepo(db *dynamodb.Client) *OrderRepo {
 	return &OrderRepo{db}
 }
 
-func (o OrderRepo) CreateOrder(body *pb.CreateOrderRequest) (*pb.OrderResponse, error) {
-	orderId, _ := uuid.NewUUID()
-
-	order := Order{
-		PK:         fmt.Sprintf("ORDER#%s", orderId),
-		SK:         fmt.Sprintf("ORDER#%s", orderId),
-		CustomerID: body.CustomerId,
-		SellerID:   body.SellerId,
-		Product: Product{
-			ProductID:   body.Product.ProductId,
-			ProductName: body.Product.ProductName,
-		},
-		PaymentMethod:    body.PaymentMethod.String(),
-		DeliveryLocation: body.DeliveryLocation,
-		OrderStatus:      "ORDER_CREATED",
-		CreatedTimestamp: time.Now().String(),
-		UpdatedTimestamp: time.Now().String(),
-	}
-
+func (o OrderRepo) CreateOrder(order Order) (*pb.OrderResponse, error) {
 	av, err := attributevalue.MarshalMap(order)
 
 	if err != nil {
-		return nil, err
+		return nil, errors.New("error while marshelling")
 	}
 
 	_, err = o.db.PutItem(context.TODO(), &dynamodb.PutItemInput{
@@ -70,8 +50,8 @@ func (o OrderRepo) CreateOrder(body *pb.CreateOrderRequest) (*pb.OrderResponse, 
 	})
 
 	if err != nil {
-		return nil, err
+		return nil, errors.New("error while creating data")
 	}
 
-	return &pb.OrderResponse{OrderId: orderId.String()}, nil
+	return &pb.OrderResponse{OrderId: order.PK}, nil
 }
--- pkg/services/order.go
+++ pkg/services/order.go
@@ -2,7 +2,10 @@ package services
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/satioO/order-mgmt/pkg/models"
 	"github.com/satioO/order-mgmt/pkg/pb"
 )
@@ -19,5 +22,23 @@ func NewOrderService(repo *models.OrderRepo) *orderService {
 }
 
 func (o orderService) CreateOrder(ctx context.Context, body *pb.CreateOrderRequest) (*pb.OrderResponse, error) {
-	return o.repo.CreateOrder(body)
+	orderId, _ := uuid.NewUUID()
+
+	order := models.Order{
+		PK:         fmt.Sprintf("ORDER#%s", orderId),
+		SK:         fmt.Sprintf("ORDER#%s", orderId),
+		CustomerID: body.CustomerId,
+		SellerID:   body.SellerId,
+		Product: models.Product{
+			ProductID:   body.Product.ProductId,
+			ProductName: body.Product.ProductName,
+		},
+		PaymentMethod:    body.PaymentMethod.String(),
+		DeliveryLocation: body.DeliveryLocation,
+		OrderStatus:      "ORDER_CREATED",
+		CreatedTimestamp: time.Now().String(),
+		UpdatedTimestamp: time.Now().String(),
+	}
+
+	return o.repo.CreateOrder(order)
 }
